787f04fb3841b879e3bc14a4854023e569a09173
hipale <hhemmhilb@gmail.com>
2022-07-17 20:00:13 +0200

    Fix the gridCont bug, in which it turns into randomColor Make the wbsite more colorful using stylesheet Add new flexboxes to the site

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 let randomColor;
+let gridHori;
+let gridVert;
 let gridDivNum = 6;
+
 createNewGrid()
 function createNewGrid() {
 const gridCont = document.querySelector("#gridCont");
 for (let i = 0; i < gridDivNum; i++) {
-let gridVert = document.createElement("div");
+gridVert = document.createElement("div");
+gridVert.style.width = `${100 / gridDivNum}%`;
+gridVert.style.height = `${100 / gridDivNum}%`;
 gridVert.className = "gridDivVert";
 gridCont.appendChild(gridVert);
 for (let x = 0; x < gridDivNum - 1; x++) {
-let gridHori = document.createElement("div");
+gridHori = document.createElement("div");
 gridHori.className = "gridDivHori";
+gridHori.style.width = `${100 / gridDivNum}%`;
+gridHori.style.height = `${100 / gridDivNum}%`;
 gridCont.appendChild(gridHori);
 }
 };
-//gridVert.style.width = "100% / gridDivNum";
-//gridVert.style.height = "100% / gridDivNum";
 };
 
 gridCont.addEventListener("mouseover", colorChange);
 function colorChange(e) {
     randomColor =`#${Math.floor(Math.random()*16777215).toString(16)}`;
     let targetColor = e.target.style.backgroundColor;
-       if (targetColor !== "white") {
+       if (e.target.className !== "touched") {
         e.target.style.backgroundColor = randomColor; 
+        e.target.classList.add("touched");
        } else {
         targetColor.split("").slice(0, 1).join("");
-        e.target.style.backgroundColor = `#${targetColor - (0.1 * targetColor)}`;
+        e.target.style.backgroundColor = `#${targetColor - (targetColor * 0.1)}`;
        }
-    
-       
+       gridCont.style.backgroundColor = "white";
 }
 
 const button = document.querySelector("button");
@@ -38,13 +43,12 @@ function popUpPrompt(e) {
     if (askSizePrompt > 100) {
         alert("The grid should be smaller than 10000 divs");
     } else {
-        //removeOldGrid()
+        removeOldGrid()
         gridDivNum = askSizePrompt;
-        //createNewGrid()
+        createNewGrid()
     }
 }
-/*function removeOldGrid() {
-gridHori.remove();
-gridVert.remove();
-} */
+function removeOldGrid() {
+gridCont.innerHTML ="";
+} 
 
